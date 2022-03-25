f0fdbad09acced2ae5d4c6d276fdaee486899c7a
Gustavo Cacharuco <gustavocacharuco@gmail.com>
2022-03-25 10:02:28 -0400

    card componete

--- js/packages/web/src/views/home/components/SalesList/components/SliderCard/index.tsx
+++ js/packages/web/src/views/home/components/SalesList/components/SliderCard/index.tsx
@@ -1,12 +1,21 @@
-import React,{useEffect, useState } from 'react';
+import React,{useEffect, useRef, useState, MutableRefObject } from 'react';
 import { CardLoader } from '../../../../../../components/MyLoader';
 import {Row,Col} from'antd';
 import { Button} from 'antd';
 import { RightOutlined, LeftOutlined } from '@ant-design/icons';
+import CardComponent from '../Card';
+
+export interface Card {
+    pubkey: string;
+    image:string;
+    name:string;
+    description:string;
+}
 
 export const SliderCard =()=>{
-    const [cardDat,setCardDat]=useState();
+    const [cardDat,setCardDat]=useState([]);
     const [isLoanding,setIsLoading] = useState(true);
+    const sliderCard = useRef() as MutableRefObject<HTMLInputElement>;
     useEffect(()=>{
         const onLoad = async () => {
            let resp =  await fetch('https://apinft.proit.studio/all_nfts')
@@ -15,21 +24,33 @@ export const SliderCard =()=>{
            setIsLoading(false);
            
           };
-          window.addEventListener('load', onLoad);
-          return () => window.removeEventListener('load', onLoad);
-    },[])
+          onLoad()
+          /* window.addEventListener('load', onLoad);
+          return () => window.removeEventListener('load', onLoad);*/
+    },[]) 
+
+    const handleClickRigth =(event)=>{
+        sliderCard.current.scrollLeft += 10;
+        console.log(sliderCard)
+        console.log(cardDat)
+    }
+    const handleClickLeft =(event)=>{
+        sliderCard.current.scrollLeft -= 10;
+        console.log(sliderCard.current.offsetWidth)
+    }
     return(
-        <>{!isLoanding && <div className="slider-wrap">
-            {  [...Array(5)].map((_, idx) => <Col span={5}><CardLoader key={idx} width={200} height={370} /></Col>)}
-           </div> }
-           {!isLoanding && <div className="slider-wrap">
-            {  [...Array(5)].map((_, idx) => <Col span={5}><CardLoader key={idx} width={200} height={370} /></Col>)}
-            
-           </div> }
-        
-           <Button type="primary" shape="circle">
+        <><div className="slider-wrap" ref={sliderCard}>
+           {isLoanding &&   [...Array(5)].map((_, idx) => <Col span={5}><CardLoader key={idx} width={200} height={370} /></Col>)}
+           {!isLoanding &&  cardDat.map((c:Card, idx) => <Col span={5}><CardComponent key={idx} description={c.description} posterUri={c.image} name={c.name}  pubkey={c.pubkey} /></Col>)}
+           
+           </div>
+           <Button type="primary" className="sliderCard-btnLeft" shape="circle" onClick={handleClickLeft} >
+                <LeftOutlined />
+            </Button>
+           <Button type="primary" className="sliderCard-btnRigth" shape="circle" onClick={handleClickRigth} >
                 <RightOutlined />
             </Button>
+            
         </>
     )
 }
